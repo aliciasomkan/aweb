56f99a378a734205b80af379588466c2d9902c32
aliciasomkan <aliciaallensomkan@gmail.com>
2024-11-10 13:28:44 +0100

    Add files via upload

---
+++ PracticaJavaScript.js
@@ -0,0 +1,19 @@
+/*
+Ejemplo de fichero con java
+*/
+function Pulsar1(){
+    
+document.getElementById('boton1').innerHTML="Muy bien, lo has pulsado :)";
+document.getElementById('boton1').style.fontSize = "30px"
+document.getElementById('boton1').style.backgroundColor ="pink"
+document.getElementById('boton1').style.padding = "20px"
+
+}
+
+function Pulsar2(){
+    var boton = document.getElementById('boton2')
+    boton.innerHTML="Muy bien, lo has pulsado";
+    boton.style.fontSize ="30px"
+    boton.style.backgroundColor ="pink"
+    boton.style.padding ="20px"
+}
